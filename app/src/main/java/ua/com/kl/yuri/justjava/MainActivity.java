98c703ee361a995f190d7c4e032090d8e09dcea3
Yuri <creator.and.dev@gmail.com>
2018-02-13 01:47:54 +0200

    + added Localization (ru and en) value, value-ru

--- app/src/main/java/ua/com/kl/yuri/justjava/MainActivity.java
+++ app/src/main/java/ua/com/kl/yuri/justjava/MainActivity.java
@@ -188,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method displays the given quantity value on the screen.
-     */
+     * */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
